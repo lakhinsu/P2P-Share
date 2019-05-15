4bd7e0aa9633cd4d1686aa9a10e7eff907ddac9b
lakhinsu <hinsulak@gmail.com>
2019-05-15 17:30:08 +0530

    Electron setup comlete and ready to code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Alert , Jumbotron } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+  constructor(props) {
+    super(props);
 
+    this.state = { show: true };
+  }
+  render() {
+    const handleHide = () => this.setState({ show: false });
+    const handleShow = () => this.setState({ show: true });
+    return (
+     <>
+      //code goes here 
+     </>
+
+    );
+        }
+      }
+      
 export default App;
